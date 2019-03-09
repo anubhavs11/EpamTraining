5db09700cccbaafe2e5da2c4b6b086af2178d31f
Anubhav Singh <anubhavsingh5132506@gmail.com>
2019-03-09 13:44:44 +0530

    new commit

--- git-demo/Demo.java
+++ git-demo/Demo.java
@@ -3,6 +3,6 @@ public class Demo{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		int a=sc.nextInt();
-		System.out.println("You just entered : "+a);
+		System.out.println("Hello, You have just entered : "+a);
 	}
 }
