58811658f27cfb1cc67944997af42d16dc9ec6ba
Subhradeep Bhattacharya <37349689+iamsubhro@users.noreply.github.com>
2020-10-31 10:25:40 -0700

    Update HelpOutIndianArmy.java

--- Epam-task3-19March/HelpOutIndianArmy.java
+++ Epam-task3-19March/HelpOutIndianArmy.java
@@ -1,109 +1,79 @@
+package algorithms;
 
-/**
- * Problem Description
- * https://www.hackerearth.com/practice/algorithms/greedy/basics-of-greedy-algorithms/practice-problems/algorithm/help-out-the-indian-army/description/
- * @author anubhav
- *
- */
 import java.util.*;
-class TestClass {
-    public static void main(String args[] ) throws Exception {
 
-    // list for left and right safe distance for every checkpoint
-    List<Long> list1=new ArrayList<Long>();
-    List<Long> list2=new ArrayList<Long>();
-    
-    int N;
-	
-	long S, E, x, d;
-	long left, right, ans;
-	
-	Scanner sc = new Scanner(System.in);
-	N = sc.nextInt();
-	S = sc.nextLong();
-	E = sc.nextLong();
-	
-	// iterating Every checkpoint
-	for(int i=0; i<N;i++)
-	{
-		x = sc.nextLong();
-	    d = sc.nextLong();
-		
-		// as this is the range the army is protected 
-		left = x-d;  
-		
-		// so we calculate the range by calculating left value and right 
-		right = x+d;  
-		
-		if ((right<=S) || (left>=E))      
-			continue;
-		// as we comparing the values for because if the value of left is less than 
-		left = Math.max(left, S);        
-		 
-	    // the S value then we will include the value of S
-		right = Math.min(right, E);    
-		
-		// make pair of elements 
-		list1.add(left);
-		list2.add(right);
+class pair implements Comparable<pair> {
+	Long s, e;
+ 
+	pair(Long s, Long e) {
+		this.s = s;
+		this.e = e;
 	}
-	
-	
-	if(list1.size()==0)
-	{
-		System.out.println(E-S);
+ 
+	public int compareTo(pair o) {
+		return s.compareTo(o.s);
 	}
-	
-	/**
-	 * Sorting list1 and updating corresponding values in list2 
-	 * So as to start from ordered range
-	 */
-	//creating HashMap for list1 and list2
-    HashMap<Long,Long> pair = new HashMap<Long,Long>(); 
-    for(int i=0;i<list1.size();i++){
-        pair.put(list1.get(i),list2.get(i));    
-    }
-    //Sorting the HashMap keys i.e. list1
-	Collections.sort(list1);
-	// updating corresponding values of list2
-	for(int i=0;i<list1.size();i++){
-        list2.set(i,pair.get(list1.get(i)));
-    }
-	
-	// exits the system if no elements found in the lists
-	if(list1.size()==0){
-	    System.exit(0);
-	}
-	// we taken the first value
-	left = list1.get(0); 
-	
-	 // we take the second value
-	right = list2.get(0);
-	
-	// calculated the difference from smallest range, so the difference
-	// will be added in answer
-	ans = Math.max(0L, left-S);  
-    
-    //iterating all the list elements
-	for(int i=1;i<list1.size();i++)
-	{
-	    // checks if left safest place is greater than right safest place of previous checkpost
-		if(list2.get(i)>right)
-		{
-		    //updating total unsafe distance
-			ans = ans + Math.max(0L, list1.get(i) - right);
-			
-			/** updating previous checkpoint right safest place as
-			*   current checkpoint right safest place
-			*/
-			right = list2.get(i);
+}
+ 
+class HelpoutIndianArmy {
+	public static void main(String args[]) throws Exception {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Long startpos = sc.nextLong();
+		Long endpos = sc.nextLong();
+		pair p[] = new pair[n];
+		for (int i = 0; i < n; i++) {
+			Long x = sc.nextLong();
+			Long y = sc.nextLong();
+			p[i] = new pair(x - y, x + y);
 		}
+		System.out.println(solve(p,startpos,endpos,n));
+		sc.close();
 	}
-	// adding unsafe distance between last checkpost's rightmost safest place
-	// to the last destination i.e. E
-	ans = ans + Math.max(0L, E - right); 
-	
-	//printing total unsafe distance b/w E and S
-	System.out.println(ans); 
+	static long solve(pair p[],Long startpos,Long endpos,int n)
+		{
+		Arrays.sort(p);
+		Long s = p[0].s;
+		Long e = p[0].e;
+		ArrayList<pair> a = new ArrayList<>();
+		for (int i = 1; i < n; i++) {
+			if (p[i].s <= e) {
+				e = Math.max(p[i].e, e);
+			} else {
+				a.add(new pair(s, e));
+				s = p[i].s;
+				e = p[i].e;
+ 
+			}
+		}
+		a.add(new pair(s, e));
+		long ans = 0;
+		for (int i = 0; i < a.size(); i++) {
+			if (startpos >= endpos) {
+				startpos = endpos;
+				break;
+ 
+			}
+			if (startpos >= a.get(i).s && startpos <= a.get(i).e) {
+				startpos = a.get(i).e;
+			} else if (startpos <= a.get(i).s && endpos >= a.get(i).e) {
+				ans += a.get(i).s - startpos;
+				startpos = a.get(i).e;
+			} else if (startpos <= a.get(i).s && endpos >= a.get(i).s && endpos <= a.get(i).e) {
+				ans += a.get(i).s - startpos;
+				startpos = endpos;
+			}
+ 
+			else if (startpos <= a.get(i).s && endpos <= a.get(i).s) {
+				ans += endpos - startpos;
+				startpos = endpos;
+			}
+ 
+		}
+		if (startpos < endpos) {
+			ans += endpos - startpos;
+		}
+		return ans;
 	}
 }
